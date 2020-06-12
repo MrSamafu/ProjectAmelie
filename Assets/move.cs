cbdfab8a7f24725a224de741f25e8eb9c9f551ed
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-06-12 18:05:25 +0200

    init with map

---
+++ Assets/move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 1 , transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+     
+        }
+        
+    }
+}
